4154d75df2c838436dc7df95cefab700c416e21d
LFDM <1986gh@gmail.com>
2013-12-01 17:08:19 +0100

    Activates multi-threaded segtok method.

---
+++ lib/llt/api.rb
@@ -0,0 +1,49 @@
+require 'llt'
+require 'llt/core/api'
+require 'sinatra/base'
+require 'sinatra/respond_with'
+
+class Api < Sinatra::Base
+  helpers LLT::Core::Api::Helpers
+  register Sinatra::RespondWith
+
+  get '/segtok' do
+    text = params[:text].to_s
+    seg = LLT::Segmenter.new(params)
+    tok = LLT::Tokenizer.new(params)
+    sentences = seg.segment(text)
+    if sentences.any?
+      threads_count = params[:threads] || 4
+      threads = []
+      sentences.each_slice(slice_size(sentences, threads_count)) do |sliced|
+        threads << Thread.new do
+          forked_tok = tok.fork_instance
+          process_segtok(forked_tok) do
+            sliced.each do |sentence|
+              forked_tok.tokenize(sentence.to_s, add_to: sentence)
+            end
+          end
+        end
+      end
+
+      threads.each(&:join)
+    end
+
+    respond_to do |f|
+      f.xml { to_xml(sentences, params) }
+    end
+  end
+
+  def slice_size(sentences, threads)
+    size = sentences.size / threads - 1
+    size < 0 ? (sentences.size - 1) : size
+  end
+
+  def process_segtok(forked_tok)
+    if forked_tok.db.type == :prometheus
+      StemDatabase::Db.connection_pool.with_connection { yield }
+    else
+      yield
+    end
+  end
+end
---
+++ spec/lib/llt/api_spec.rb
@@ -0,0 +1,70 @@
+ENV['RACK_ENV'] = 'test'
+
+require 'spec_helper'
+require 'llt/api'
+require 'rack/test'
+
+def app
+  Api
+end
+
+describe "main api" do
+  include Rack::Test::Methods
+
+  describe '/segtok' do
+    context "with URI as input" do
+      it "responds to GET" do
+        get '/segtok'
+        last_response.should be_ok
+      end
+    end
+
+    let(:text) {{text: "homo mittit. Marcus est."}}
+
+    context "with text as input" do
+      context "with accept header json" do
+        it "segtoks the given text" do
+          pending
+          get '/segtok', text,
+            {"HTTP_ACCEPT" => "application/json"}
+          last_response.should be_ok
+          response = last_response.body
+          parsed_response = JSON.parse(response)
+          parsed_response.should have(3).items
+        end
+      end
+
+      context "with accept header xml" do
+        it "segtoks the given text" do
+          pending
+          get '/segtok', text,
+            {"HTTP_ACCEPT" => "application/xml"}
+          last_response.should be_ok
+          body = last_response.body
+          body.should =~ /<w>homo<\/w>/
+          body.should =~ /<w>mittit<\/w>/
+          body.should =~ /<pc>\.<\/pc>/
+        end
+
+        it "receives params for tokenization and markup" do
+          params = {
+            indexing: true,
+            recursive: true,
+            inline: true,
+          }.merge(text)
+
+          get '/segtok', params,
+            {"HTTP_ACCEPT" => "application/xml"}
+          last_response.should be_ok
+          body = last_response.body
+          body.should =~ /<w s_n="1" n="1">homo<\/w>/
+          body.should =~ /<w s_n="1" n="2">mittit<\/w>/
+          body.should =~ /<pc s_n="1" n="3">\.<\/pc>/
+          body.should =~ /<w s_n="2" n="1">Marcus<\/w>/
+          body.should =~ /<w s_n="2" n="2">est<\/w>/
+          body.should =~ /<pc s_n="2" n="3">\.<\/pc>/
+        end
+      end
+    end
+  end
+end
