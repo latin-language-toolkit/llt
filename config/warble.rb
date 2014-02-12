d8656ce99394d494852793fce8db95d3a26cac9e
LFDM <1986gh@gmail.com>
2014-02-12 23:12:17 +0100

    Starts to use bundler for warbling

--- config/warble.rb
+++ config/warble.rb
@@ -3,16 +3,8 @@
 
 # Warbler web application assembly configuration file
 Warbler::Config.new do |config|
-  # We are using the gemspec instead, as the Gemfile itself is
-  # pointing to edge versions of dependent gems. It also seems
-  # that warbler has all kinds of troubles when pulling in gems
-  # from git...
-  config.bundler = false
-
-  # These gems are jruby only, we cannot include them in the gemspecs, as they
-  # would break MRI compatibility.
-  config.gems << 'activerecord-jdbcpostgresql-adapter'
-  config.gems << 'jruby-httpclient'
+  # This is finally working with the advent of warbler version 1.4.1
+  config.bundler = true
 
   # Set JRuby to run in 2.0 mode.
   config.webxml.jruby.compat.version = "2.0"
