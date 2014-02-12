d0dd2501ee870e356fcce4a13b81a1714abc4705
LFDM <1986gh@gmail.com>
2014-02-13 00:06:26 +0100

    Deprecates gem updates, uses bundler instead

--- lib/llt/cli.rb
+++ lib/llt/cli.rb
@@ -18,7 +18,12 @@ module LLT
       check_ruby_version
 
       inside BASE_DIR do
-        update_gems(llt_gems)
+        # This is deprecated for now - and probably forever, as we now can use
+        # bundler for warbling and live on the edge of all master branches
+        # We just update the bundle instead
+        #update_gems(llt_gems)
+
+        update_bundle
         run_warbler
         deploy_to_tomcat(options)
         reseed_prometheus_stems(options)
@@ -26,18 +31,9 @@ module LLT
     end
 
     no_commands do
-      def llt_gems
-        File.read('Gemfile').each_line.with_object([]) do |line, arr|
-          arr << $1 if line.match(/^gem '(llt-.*?)'/)
-        end
-      end
-
-      def update_gems(gems)
-        say_status('updating', '')
-        gem_list = `gem list`
-        to_update, to_install = gems.partition { |gem| gem_list.match(/#{gem}/)}
-        system "gem install #{to_install.join(' ')}" if to_install.any?
-        system "gem update #{to_update.join(' ')}"
+      def update_bundle
+        say_status('updating', 'trough bundle update')
+        `bundle update`
       end
 
       def run_warbler
@@ -48,10 +44,14 @@ module LLT
       def deploy_to_tomcat(options)
         if tomcat = options[:tomcat]
           app_dir = File.join(tomcat, 'webapps')
-          say_status(:copying, "llt.war => #{app_dir}")
-          system("cp llt.war #{app_dir}")
+          say_status(:copying, "#{war_name} => #{app_dir}")
+          system("cp #{war_name} #{app_dir}")
           restart(tomcat) if options[:restart_server]
         end
+      end
+
+      def war_name
+        "llt.war"
       end
 
       def restart(tomcat)
@@ -74,6 +74,20 @@ module LLT
           exit
         end
       end
+
+      #def llt_gems
+        #File.read('Gemfile').each_line.with_object([]) do |line, arr|
+          #arr << $1 if line.match(/^gem '(llt-.*?)'/)
+        #end
+      #end
+
+      #def update_gems(gems)
+        #say_status('updating', '')
+        #gem_list = `gem list`
+        #to_update, to_install = gems.partition { |gem| gem_list.match(/#{gem}/)}
+        #system "gem install #{to_install.join(' ')}" if to_install.any?
+        #system "gem update #{to_update.join(' ')}"
+      #end
     end
   end
 end
