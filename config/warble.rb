b65ecedd8db3d8294fb5b6e89e2f172d8b4b2ea1
LFDM <1986gh@gmail.com>
2014-02-13 00:07:15 +0100

    Hardcodes name of war file

--- config/warble.rb
+++ config/warble.rb
@@ -8,4 +8,8 @@ Warbler::Config.new do |config|
 
   # Set JRuby to run in 2.0 mode.
   config.webxml.jruby.compat.version = "2.0"
+
+  # also used to name the war - defaults to the name and a version string
+  # right now, which is not that useful
+  config.jar_name = 'llt'
 end
